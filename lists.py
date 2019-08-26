f61520df15c68583e993202bd76eef3b794cebfe
Mary West <westmarye1219@gmail.com>
2019-08-26 12:16:13 -0500

    planets

---
+++ lists.py
@@ -0,0 +1,17 @@
+import random
+
+my_randoms = [1, 3, 1, 2, 4, 2, 5, 4, 2, 5]
+
+my_randoms = list()
+for i in range(10):
+    my_randoms.append(random.randrange(1, 6, 1))
+
+my_randoms.append()
+
+
+"""
+Print a message to the console indicating whether each value of
+`number` is in the `my_randoms` list.
+"""
+for number in range(1, 6):
+  print(number)
